7b6832e7fb47af04873c90388cdeebfd773749d3
Emerson66 <emersonreisdacosta@gmail.com>
2021-10-16 16:39:47 -0400

    projeto criado

---
+++ src/test/java/br/com/buritiscript/launcher/LauncherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.launcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
